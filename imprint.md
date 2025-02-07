7486d3051abb3ad783c82dbe9b98bf6a729ccd94
Ariel Simulevski <ariel@simulevski.at>
2025-02-07 16:51:27 +0100

    Add min height

---
+++ imprint.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: Imprint
+permalink: /imprint/
+---
+
+Auralis Group GmbH · Münchner Str. 25, 85229 Markt Indersdorf · HRB 296015, Amtsgericht München
+Managing Directors: Daniel Ratke, Marcel Stapelfeldt · info@auralispartners.com 
--- scripts/default.js
+++ scripts/default.js
@@ -1,5 +1,15 @@
 import { annotate } from 'rough-notation';
 
-const header = document.querySelector('#content-header > h1');
-const annotation = annotate(header, { type: 'underline', color: '#086788', multiline: true });
-annotation.show();
+if (document.querySelector('#content-header')) {
+    const header = document.querySelector('#content-header > h1');
+    const annotation = annotate(header, { type: 'underline', color: '#086788', multiline: true });
+    annotation.show();
+}
+
+const footer = document.querySelector('#footer');
+const content = document.querySelector('#content');
+
+const contentPosition = content.getBoundingClientRect();
+
+content.style.minHeight = `${window.innerHeight - contentPosition.top - footer.clientHeight}px`;
+footer.style.display = 'block';
